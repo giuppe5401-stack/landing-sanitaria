965d43ba71472bcb15db4a6bd18c46e36c4c8fcf
giuppe5401-stack <giuppe5401@gmail.com>
2025-09-06 01:13:33 +0200

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AssistenzaSanitariaLanding from './AssistenzaSanitariaLanding'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AssistenzaSanitariaLanding />
+  </React.StrictMode>,
+)
